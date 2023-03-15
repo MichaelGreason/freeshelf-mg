4e7852ddc5f00ded2f1c39f24443dd826f14cee0
MichaelGreason <mgreason18@gmail.com>
2023-03-14 20:30:06 -0400

    added a function in App.js to call on the bookData.json

--- src/App.js
+++ src/App.js
@@ -5,8 +5,23 @@ function App() {
   return (
     <div className="section">
       <h1>Freeshelf</h1>
-      {/* Use bookData to show books on the page*/}
+      <ul>
+        {bookData.map(book => {
+          return <BD book={book} />
+        })}
+      </ul>
     </div>
+  )
+}
+
+
+function BD({ book }) {
+  return (
+    <>
+      <h4>{book.title}</h4>
+      <p>{book.author}</p>
+      <div>{book.shortDescription}</div>
+    </>
   )
 }
 
