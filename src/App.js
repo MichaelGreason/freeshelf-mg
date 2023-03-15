7578e110444ccfd2e31dbb2a27a3f13f4cdb6b8f
MichaelGreason <mgreason18@gmail.com>
2023-03-15 16:13:16 -0400

    All info shows if it is available, more minor CSS changes

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ function App() {
   return (
     <div className="section">
       <h1 className='freeshelf'>Freeshelf</h1>
-      <ul>
+      <div>
         {bookData.map(book => {
           return <BD book={book} />
         })}
-      </ul>
+      </div>
     </div>
   )
 }
@@ -21,27 +21,33 @@ function App() {
 
 function BD({ book }) {
   const [expanded, setExpanded] = useState(false)
+  const [showImage, setShowImage] = useState(true);
+  const hideImg = (event) => {
+    setShowImage(false);
+  }
+
   return (
-    <>
-      <div className='bigbox'>
+  <>
+  <div className='all'>
+    <div className='bigbox'>
       <h4 className='title'>{book.title}</h4>
-      <p><strong>{book.author}</strong></p>
-      <div className='short-image'>
-        <div className='shortD'>{book.shortDescription}</div>
-        <div className='image'>{<img src={book.coverImageUrl}> ? <img src={book.coverImageUrl} alt="cover" width="125" height="175"/> : ''}</div>
-      </div>
+        <p><strong>{book.author}</strong></p>
+            <div className='shortD'>{book.shortDescription}</div>
       <FontAwesomeIcon className='arrow' icon={expanded ? faAnglesDown : faAnglesRight} onClick={() => setExpanded(!expanded)} />
       {
         expanded && 
-      <div><p><strong>URL: </strong><a href={book.url}>{book.url}</a></p>
-        <p >{book.publisher ? `Publisher: ${book.publisher}` : ''}</p>
-        <p><strong>Publication Date: </strong>{book.publicationDate}</p>
+        <div><p><strong>URL: </strong><a href={book.url}>{book.url}</a></p>
+        {book.publisher ? <p><strong>Publisher: </strong>{book.publisher}</p> : ''}
+        {book.publicationDate ? <p><strong>Publication Date: </strong>{book.publicationDate}</p> : ''}
         <p><strong>Full Description: </strong>{book.detailedDescription}</p>
     </div>
       }
       </div>
+      <div className='imageBox'>
+      <div className='image'>{showImage ? (<img src={book.coverImageUrl} alt="cover" width="80%" height="80%" onError={hideImg}/>) :  ''}</div>
+      </div>
+    </div>
       </>
-
   )
 }
 
