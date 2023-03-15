69a2e5d8e99f12228704083a0677a8a90bb00320
MichaelGreason <mgreason18@gmail.com>
2023-03-15 14:34:20 -0400

    broken atm

--- src/App.js
+++ src/App.js
@@ -23,17 +23,18 @@ function BD({ book }) {
   const [expanded, setExpanded] = useState(false)
   return (
     <>
-    <div className='bigbox'>
+      <div className='bigbox'>
       <h4 className='title'>{book.title}</h4>
       <p><strong>{book.author}</strong></p>
       <div className='short-image'>
         <div className='shortD'>{book.shortDescription}</div>
-      <div className='image'><img src={book.coverImageUrl} alt="coverImage" width="125" height="175"/></div>
+        <div className='image'>{<img src={book.coverImageUrl}> ? <img src={book.coverImageUrl} alt="cover" width="125" height="175"/> : ''}</div>
       </div>
       <FontAwesomeIcon className='arrow' icon={expanded ? faAnglesDown : faAnglesRight} onClick={() => setExpanded(!expanded)} />
       {
-        expanded && <div><p><strong>URL: </strong><a href={book.url}>{book.url}</a></p>
-        <p><strong>Publisher: </strong>{book.publisher}</p>
+        expanded && 
+      <div><p><strong>URL: </strong><a href={book.url}>{book.url}</a></p>
+        <p >{book.publisher ? `Publisher: ${book.publisher}` : ''}</p>
         <p><strong>Publication Date: </strong>{book.publicationDate}</p>
         <p><strong>Full Description: </strong>{book.detailedDescription}</p>
     </div>
