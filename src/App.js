bcaae07d6ca5b7be790b76c6020a4e88265b55b4
MichaelGreason <mgreason18@gmail.com>
2023-03-14 22:09:13 -0400

    added all text elemnt to the page including expanded menu

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css'
 import bookData from './book-data.json'
+import { useState } from 'react'
 
 function App() {
   return (
@@ -16,12 +17,21 @@ function App() {
 
 
 function BD({ book }) {
+  const [expanded, setExpanded] = useState(false)
   return (
     <>
       <h4>{book.title}</h4>
-      <p>{book.author}</p>
+      <p><strong>{book.author}</strong></p>
       <div>{book.shortDescription}</div>
-    </>
+      <button onClick={() => setExpanded(!expanded)}>MORE</button>
+      {
+        expanded && <div><p><strong>URL: </strong><a>{book.url}</a></p>
+        <p><strong>Publisher: </strong>{book.publisher}</p>
+        <p><strong>Publication Date: </strong>{book.publicationDate}</p>
+        <p><strong>Full Description: </strong>{book.detailedDescription}</p>
+    </div>
+      }
+      </>
   )
 }
 
